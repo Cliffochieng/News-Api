8ef4ad983d1b14efabc8962031b70e3c687b6657
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-21 13:26:27 +0300

    add an app routr for cnn in views.py

--- app/main/views.py
+++ app/main/views.py
@@ -55,3 +55,28 @@ def bbc():
     mylist = zip(news, desc, url, img)
 
     return render_template('bbc.html', context=mylist)
+
+    @app.route('/cnn')
+def cnn():
+    newsapi = NewsApiClient(api_key="d40c178ee0c6474f963ed683b651a3ab")
+    topheadlines = newsapi.get_top_headlines(sources="cnn")
+
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    url = []
+    img = []
+   
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        url.append(myarticles['url'])
+        img.append(myarticles['urlToImage'])
+
+    mylist = zip(news, desc, url, img)
+
+    return render_template('cnn.html', context=mylist)
