9a4576f9ae85223e7e0f09a17b65dc68cbd3a259
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-21 14:00:07 +0300

    add a fucntion to get the jsaon response to the url request

--- app/request.py
+++ app/request.py
@@ -3,3 +3,27 @@ import urllib.request
 import json
 from .models import News
 
+News = news.News
+
+#getting the Api key
+api_key = app.config['NEWS_API_KEY']
+
+
+def get_news(source):
+    '''
+    Function that gets the json responce to our url request
+    '''
+    get_news_url = api_key
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_movies_response['results']:
+            news_results_list = get_news_response['results']
+            news_results = process_results(news_results_list)
+
+    return news_results
+
