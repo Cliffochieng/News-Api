a393f063f33763aee88d49e23490d24045acd87f
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-21 14:22:55 +0300

    import unittest and News in the tests

---
+++ tests/news_test.py
@@ -0,0 +1,13 @@
+import unittest
+from app.models import News
+
+class News:
+    '''
+    News class to define News Objects
+    '''
+
+    def __init__(self,title, description, url, urlToImage):
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
