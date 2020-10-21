1b3d79c8dfd570f079a8edc8ce933ad4bf63749e
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-21 13:25:05 +0300

    add an app routr for bbc in views.py

--- app/main/views.py
+++ app/main/views.py
@@ -31,3 +31,27 @@ def index():
 
     return render_template('index.html', context=mylist)
 
+@app.route('/bbc')
+def bbc():
+    newsapi = NewsApiClient(api_key="d40c178ee0c6474f963ed683b651a3ab")
+    topheadlines = newsapi.get_top_headlines(sources="bbc-news")
+
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    url = []
+    img = []
+    
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        url.append(myarticles['url'])
+        img.append(myarticles['urlToImage'])
+
+    mylist = zip(news, desc, url, img)
+
+    return render_template('bbc.html', context=mylist)
