f1b20583443cd6b42587a40abca8a61bfab2015f
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-21 13:23:20 +0300

    add index app route in views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,2 +1,33 @@
 from flask import Flask, render_template
 from newsapi import NewsApiClient
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    newsapi = NewsApiClient(api_key="d40c178ee0c6474f963ed683b651a3ab")
+    topheadlines = newsapi.get_top_headlines(sources="abc-news")
+
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    url = []
+    img = []
+    
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        url.append(myarticles['url'])
+        img.append(myarticles['urlToImage'])
+        
+
+    mylist = zip(news, desc, url, img)
+
+    return render_template('index.html', context=mylist)
+
